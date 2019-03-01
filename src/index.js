8384ef84dfe1ef9c6912fa0915be980e06a7d6ec
Stéphane Beaudet <stephaneBlitz@gmail.com>
2019-03-01 11:27:53 -0500

    set the basename in the router

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import "./styles.css";
 
 function App() {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <div>
                 <PrivatePage path="/hello" exact component={HelloUserPage} />
                 <Route path="/login" exact component={LoginPage} />
